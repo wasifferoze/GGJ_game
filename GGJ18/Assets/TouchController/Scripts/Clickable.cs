839c7185b067c3dd9097df2d4489bda3137b6c32
wasifferoze <wasif_feroze@yahoo.com>
2018-01-28 14:33:46 +0800

    clickable image

---
+++ GGJ18/Assets/TouchController/Scripts/Clickable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Clickable : MonoBehaviour , IPointerClickHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		if (Input.GetButtonDown("Fire1"))
+//		{
+//			Debug.Log(Input.mousePosition);
+//		}
+//	}
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		Debug.Log(Input.mousePosition);
+	}
+}
